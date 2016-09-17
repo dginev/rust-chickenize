bd7874e4640fa88850a69768b81fedad4e8c3347
Deyan Ginev <d.ginev@jacobs-university.de>
2016-09-17 12:18:53 +0300

    update README with new features

--- tests/basic_test.rs
+++ tests/basic_test.rs
@@ -45,3 +45,23 @@ fn lorem_ipsum_string() {
   let hello = "hello again, my friend.";
   assert_eq!(hello.lorem_ipsum(), "Lorem ipsum, dolor sit.");
 }
+
+#[test]
+fn readme_example_test() {
+  let example = "The Vice-president didn't like his style.";
+  let chickenized = example.chicken();
+  assert_eq!(chickenized,
+             "chicken chicken-chicken chicken'chicken chicken chicken chicken.");
+
+  let buffalaxed = example.buffalo();
+  assert_eq!(buffalaxed,
+             "buffalo buffalo-buffalo buffalo'buffalo buffalo buffalo buffalo.");
+
+  let anonymized = example.anonymize("secret");
+  assert_eq!(anonymized,
+             "secret secret-secret secret'secret secret secret secret.");
+
+  let lorem_ipsum = example.lorem_ipsum();
+  assert_eq!(lorem_ipsum,
+             "Lorem ipsum-dolor sit'amet consectetur adipiscing elit.");
+}
