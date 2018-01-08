948684b62b49610c2d36cbbd0a1ebe9278ad4784
Deyan Ginev <d.ginev@jacobs-university.de>
2018-01-07 20:56:01 -0500

    convenience utilities

---
+++ bin/chickenize.rs
@@ -0,0 +1,26 @@
+extern crate chickenize;
+use chickenize::*;
+
+use std::env;
+use std::fs;
+use std::io;
+
+// To enter text from stdin:
+// cargo run --bin chickenize -
+//
+// To read from a file:
+// cargo run --bin chickenize file.txt
+
+/// A chickenize utility
+fn main() {
+  let input = env::args().nth(1).unwrap();
+  let mut rdr: Box<io::Read> = if input == "-" {
+    Box::new(io::stdin())
+  } else {
+    Box::new(fs::File::open(input).unwrap())
+  }; 
+  let mut buffer = String::new();
+  rdr.read_to_string(&mut buffer).unwrap_or_default();
+  
+  println!("{:?}",buffer.chicken())
+}
---
+++ bin/loremize.rs
@@ -0,0 +1,26 @@
+extern crate chickenize;
+use chickenize::*;
+
+use std::env;
+use std::fs;
+use std::io;
+
+// To enter text from stdin:
+// cargo run --bin loremize -
+//
+// To read from a file:
+// cargo run --bin loremize file.txt
+
+/// A loremize utility - turn a given text into a lorem ipsum stub
+fn main() {
+  let input = env::args().nth(1).unwrap();
+  let mut rdr: Box<io::Read> = if input == "-" {
+    Box::new(io::stdin())
+  } else {
+    Box::new(fs::File::open(input).unwrap())
+  }; 
+  let mut buffer = String::new();
+  rdr.read_to_string(&mut buffer).unwrap_or_default();
+  
+  println!("{:?}",buffer.lorem_ipsum())
+}
